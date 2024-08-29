5b24ded765b7adfc40a2fac920dc454e9f85e407
Moaath Alrajab <alrajam@farmingdale.edu>
2024-08-28 22:37:06 -0400

    Init commit

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,18 @@
+package org.example;
+/**
+ *
+ * @author MoaathAlrajab
+ */
+public class Student {
+
+    private String name;
+
+
+
+    //To Do 01: add a constructor to this class
+
+    // To Do 02: Add setters and getters
+
+
+
+}
